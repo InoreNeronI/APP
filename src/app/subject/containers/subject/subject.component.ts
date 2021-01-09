de80c4efe420526c098fcef29f0471e8deb60de6
mmozos <martinmozos@gmail.com>
2021-01-09 10:30:28 +0100

    Approach to translate subject name TODO: Make working when switching language

--- src/app/subject/containers/subject/subject.component.ts
+++ src/app/subject/containers/subject/subject.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {SubjectService} from '../../services/subject.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-subject',
@@ -13,13 +14,14 @@ export class SubjectComponent implements OnInit {
 
   constructor(
     public route: ActivatedRoute,
-    public subjectService: SubjectService
+    public subjectService: SubjectService,
+    public translate: TranslateService
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       console.log(params)
-      this.subject = params.get("subject")
+      this.translate.get(params.get('subject').toUpperCase()).subscribe((text:string) => this.subject = text);
     });
 
     this.subjectService.getSubjects({page: 1}).subscribe(data => {
