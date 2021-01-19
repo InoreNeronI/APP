dcb04759f14d7237b44f098f1cd26b08e3d454d4
Joseba Madrigal <mjoseba@gmail.com>
2021-01-19 22:36:06 +0100

    admin buttons style fix admin link fix

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -67,6 +67,7 @@ export class AuthService {
 
   logout(): void {
     localStorage.removeItem('currentUser');
+    localStorage.removeItem('currentRole');
     this.router.navigate(['/auth']);
   }
 
