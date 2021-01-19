211c99c346c85a65e88a5bd3591a546d5492d460
mmozos <martinmozos@gmail.com>
2021-01-19 14:14:16 +0100

    Production ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+
+const basePublicUrl = 'http://pc2.labs.birt.eus/back-end/api';
+const baseUrl = 'http://pc2.labs.birt.eus/back-end';
+
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: basePublicUrl,
+  api: {
+    login: `${baseUrl}/login`,
+    subject: `${basePublicUrl}/subjects`,
+    course: `${basePublicUrl}/courses`,
+    unit : `${basePublicUrl}/units`,
+    exercise: `${basePublicUrl}/exercises`,
+    user : `${basePublicUrl}/users`
+  }
 };
