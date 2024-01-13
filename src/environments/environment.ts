851cf38f196b02af1c77abeda5d0eb1e2adb7bdc
mmozos <martinmozos@gmail.com>
2024-01-13 13:21:09 +0100

    Approach bugfix 🚀

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const basePublicUrl = 'https://api.beltza.eus/api';
-const baseUrl = 'https://api.beltza.eus';
+const basePublicUrl = 'http://127.0.0.1:8000/api';
+const baseUrl = 'http://127.0.0.1:8000';
 
 export const environment = {
   production: false,
