4058a5270ee951f0fd75bab84fa3227c426ce795
mmozos <martinmozos@gmail.com>
2024-01-17 13:24:28 +0100

    Upgraded to latest and greatest

--- src/custom-webpack.config.js
+++ src/custom-webpack.config.js
@@ -1,13 +1,13 @@
 // @see https://dev.to/oyemade/improve-performance-in-an-angular-app-using-brotli-and-gzip-text-compression-2p1e
 
-const CompressionPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   plugins: [
+    new BrotliPlugin(),
     new CompressionPlugin({
       algorithm: 'gzip',
     }),
-    new BrotliPlugin(),
   ],
 };
